5f4e62ed116331bf0ac7b648f9fb924a46d1efd7
vityaman <vityaman.dev@yandex.ru>
2024-09-30 16:42:55 +0300

    #2 Add blinking led

--- lab/1-gpio/project/Core/Src/Application.cpp
+++ lab/1-gpio/project/Core/Src/Application.cpp
@@ -8,8 +8,14 @@
 #include "Application.h"
 #include "Application.hpp"
 
-void Application::OnUpdate() {
+#include "stm32f427xx.h"
+#include "stm32f4xx_hal.h"
 
+void Application::OnUpdate() {
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+	HAL_Delay(2500);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+	HAL_Delay(500);
 }
 
 void OnUpdate(application_ptr_t self) {
